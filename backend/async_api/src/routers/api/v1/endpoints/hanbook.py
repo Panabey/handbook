5c2ea2c261cfe2cae1b21d028932361f6b66c508
TheJecksMan <themarvindit@gmail.com>
2023-08-05 06:00:27 +0300

    📒 updated docs API

--- backend/async_api/src/routers/api/v1/endpoints/hanbook.py
+++ backend/async_api/src/routers/api/v1/endpoints/hanbook.py
@@ -23,17 +23,20 @@ Id = Annotated[int, Query(ge=1, le=2147483647)]
 
 @router.get("/all", response_model=list[HandbookDetail])
 async def get_handbooks(session: Session):
+    """Получение списка всех доступных справочников"""
     result = await get_all(session)
     return result
 
 
 @router.get("/content", response_model=ContentDetail)
 async def get_content_handbook(session: Session, handbook_id: Id):
+    """Получение списка всех тем и подтем справочника"""
     result = await get_content(session, handbook_id)
     return result
 
 
 @router.get("/", response_model=PageDetail)
 async def get_page_handbook(session: Session, page_id: Id):
+    """Получение содержимого подтемы справочника"""
     result = await get_page_by_id(session, page_id)
     return result
