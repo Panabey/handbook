b15d2087cfc5f35e2ed9dec6973a455ca5b8297b
TheJecksMan <themarvindit@gmail.com>
2023-08-13 07:38:32 +0300

    refactoring

--- scripts/init-user-db.sh
+++ scripts/init-user-db.sh
@@ -4,8 +4,8 @@ set -u
 
 function create_user_and_database() {
 	local database=$1
-        local user=$2
-        local password=$3
+    local user=$2
+    local password=$3
 
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 	    CREATE USER $user WITH PASSWORD '$password';
